deda6514b87a6ef0ac8ab77626859c404d16f18f
Vishavjeet6 <vishavjeetsinghthakur@gmail.com>
2020-08-04 11:14:01 +0530

    final

--- BuildingWebApplicationswithNode.jsandExpress/app.js
+++ BuildingWebApplicationswithNode.jsandExpress/app.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
-var app = express();
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
 
 // app.use(morgan('combined'));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'view', 'index.html'));
+    res.sendFile(path.join(__dirname, 'view', 'index.html'));
 });
 
-app.listen(4242, ()=>{
+app.listen(4242, () => {
     debug(`listening on port ${chalk.green('4242')}`);
 });
